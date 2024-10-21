bc24d75bdf326639248d74fe3aeae4d401a1b007
Thiago Mendes <thiagomendes.dev@gmail.com>
2024-10-20 22:03:53 -0300

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { Chat } from "@/components/Chat";
+import { ChatProvider } from "@/contexts/ChatContext";
+import { UserProvider } from "@/contexts/UserContext";
+
+const Page = () => {
+  return (
+    <div className="container mx-auto max-w-lg px-2">
+      <UserProvider>
+        <ChatProvider>
+          <h1 className="text-3xl my-3 text-center">Chat simples</h1>
+
+          <Chat />
+        </ChatProvider>
+      </UserProvider>
+    </div>
+  );
+}
+
+export default Page;
